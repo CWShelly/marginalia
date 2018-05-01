407f5dbbe6f84c7aa72d33f4e00b4734a0dc9f5c
Shelly Yusuf <ShellyNYusuf@gmail.com>
2018-05-01 01:05:06 -0700

    switching books

--- public/src/components/NoteList.js
+++ public/src/components/NoteList.js
@@ -29,54 +29,75 @@ export class NoteList extends React.Component{
 
 
 const mapStateToProps = (state)=>{
- 
+
   const json = localStorage.getItem('notes')
   const notes = JSON.parse(json);
   const book_id = localStorage.getItem('book_id');
+  console.log(book_id);
 
+console.log(state.notes);
 
     if(notes && state.notes.length == 0){
 
    console.log('notes');
         const hasBookId=(note)=>{
-          console.log(note.book_id);
+          // console.log(note.book_id);
           return note.book_id === book_id;
         }
 
-     const notes_in_this_book = notes.filter(hasBookId);
-     console.log(notes_in_this_book);
-        const notes_of_this_book = notes;
+     const notes_of_this_book = notes.filter(hasBookId);
+
+     // console.log(notes_in_this_book);
+        // const notes_of_this_book = notes;
 
       return {
         notes: notes_of_this_book.concat(state.notes)
       }
     }
     else if(notes && state.notes.length >0){
-      console.log(notes);
-      console.log(state.notes);
-      // notes and state
+
       console.log('notes and state');
 
-      // const hasBookId=(note)=>{
-      //   return note.book_id === book_id;
-      // }
- console.log(state.notes.length);
-      console.log(state.notes[state.notes.length-1]);
-   // const notes_of_this_book = notes.filter(hasBookId);
-   const notes_of_this_book = notes;
+      const hasBookId=(note)=>{
+        return note.book_id === book_id;
+      }
 
-   console.log(notes_of_this_book);
-   if(notes_of_this_book[notes_of_this_book.length-1].id ===
-state.notes[state.notes.length-1].id){
+      console.log(state.notes.length);
+      console.log(state.notes[state.notes.length-1]);
+
+   const notes_of_this_book = notes.filter(hasBookId);
+
+   const state_of_this_book = state.notes.filter(hasBookId)
+   // const notes_of_this_book = notes;
+
+      console.log(notes_of_this_book.length);
+
+   if(notes_of_this_book.length >0 && state_of_this_book.length > 0
+      && notes_of_this_book[notes_of_this_book.length-1].id ===
+     state_of_this_book[state_of_this_book.length-1].id)
+     {
+
+  console.log('yes');
   return {
     notes: notes_of_this_book
   }
 }
 else{
-  console.log(notes_of_this_book.concat([state.notes[state.notes.length-1]]));
+    console.log('no');
+    console.log(state_of_this_book.length);
+ console.log(state_of_this_book[state_of_this_book.length-1]);
+   if(state_of_this_book.length >0){
+
      return {
-       notes: notes_of_this_book.concat([state.notes[state.notes.length-1]])
+       notes: notes_of_this_book.concat([state_of_this_book[state_of_this_book.length-1]])
      }
+   }
+   else{
+     return{
+       notes: notes_of_this_book
+     }
+   }
+
    }
 }
 
