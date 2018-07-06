4cbff19f7a913e32cf65a01d38962d32ef0386ce
Shelly Yusuf <ShellyNYusuf@gmail.com>
2018-07-05 18:26:01 -0700

    continuing to set up test suite. Issues with firebase storage on test

---
+++ public/src/tests/fixtures/books.js
@@ -0,0 +1,26 @@
+import moment from 'moment'
+
+export default [
+  {
+id: 1,
+author_first_name: "Test_First_Name1",
+author_last_name: "Test_Last_Name",
+createdAt: moment(),
+owner_id: 'abc123',
+show_book: true,
+tags: {tag1: true, tag2: false},
+title: "Test Title1"
+},
+{
+id: 2,
+author_first_name: "Test_First_Name2",
+author_last_name: "Test_Last_Name2",
+createdAt: moment(),
+owner_id: 'abc123',
+show_book: true,
+tags: {tag1: true, tag2: false},
+title: "Test Title2"
+}
+
+
+];
